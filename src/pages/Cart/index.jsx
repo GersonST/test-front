3f980f656cfa5ca16e340b1d2e4e86074f9b5940
GersonST <gersonsevero90@gmail.com>
2022-02-08 01:44:22 +0000

    Consumindo API

--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -1,15 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import * as Styled from './styled'
+import axios from 'axios'
 
 function Cart() {
+  const [product, setProduct] = useState([])
+
+  useEffect(() => {
+    const GetApi = async () => {
+      await axios.get('http://www.mocky.io/v2/5b15c4923100004a006f3c07').then((response) => {
+        console.log(response.data)
+        setProduct(response.data.items)
+      })
+    }
+    GetApi()
+  }, [])
+
   return (
     <Styled.Container>
       <Styled.Produtos>Produtos</Styled.Produtos>
       <Styled.ContainerProduto>
         <ul>
-          <li>Produto 1</li>
-          <li>Produto 2</li>
-          <li>Produto 3</li>
+          {product.map((prod) => {
+            return (
+              <div key={prod.product.sku}>
+                <img src={prod.product.imageObjects[0].thumbnail} />
+                <li>{prod.product.name}</li>
+
+                <h3>R$ {prod.product.priceSpecification.originalPrice}</h3>
+              </div>
+            )
+          })}
         </ul>
       </Styled.ContainerProduto>
       <Styled.ContainerTotal>
